774df08ba5f2e2f820badc27d119b739e18cf7b1
AppsWebSolution <debkanta2019@outlook.com>
2020-12-06 13:36:35 +0530

    Updates

---
+++ precache-manifest.7b9b95bfffeb9ee2eb81fa48658ef9df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34986cdf326bc307e5ca374221b61e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fcb2f83a8231a9ab604",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "e34ddfd27942cf40aaf4",
+    "url": "/static/css/main.7891fb3b.chunk.css"
+  },
+  {
+    "revision": "6fcb2f83a8231a9ab604",
+    "url": "/static/js/2.aa5d5d68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa5d5d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34ddfd27942cf40aaf4",
+    "url": "/static/js/main.434f9f58.chunk.js"
+  },
+  {
+    "revision": "d44b690d10dbb6af349f",
+    "url": "/static/js/runtime-main.2027b445.js"
+  }
+]);
